2b01f486070b8da087f10ec1c096db6af1f69955
agomezh1901@alumno.ipn.mx <agomezh1901@alumno.ipn.mx>
2023-12-27 21:40:05 -0600

    calculadora de subneteo con c# terminada y faltan mejoras

---
+++ Subneteo/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textSub = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textClass = new System.Windows.Forms.TextBox();
+            this.textMask = new System.Windows.Forms.TextBox();
+            this.TablaSubred = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TablaSubred)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Peru;
+            this.groupBox1.Controls.Add(this.textSub);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.btnGenerar);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(25, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(456, 204);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Subneteo de subredes";
+            // 
+            // textSub
+            // 
+            this.textSub.Location = new System.Drawing.Point(198, 77);
+            this.textSub.Name = "textSub";
+            this.textSub.Size = new System.Drawing.Size(218, 20);
+            this.textSub.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(382, 44);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(56, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(299, 44);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(56, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(214, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(56, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(56, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.BackColor = System.Drawing.Color.SaddleBrown;
+            this.btnGenerar.Font = new System.Drawing.Font("Impact", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerar.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnGenerar.Location = new System.Drawing.Point(178, 142);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(141, 38);
+            this.btnGenerar.TabIndex = 6;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = false;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(361, 44);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 22);
+            this.label6.TabIndex = 5;
+            this.label6.Text = ".";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(278, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = ".";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(197, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = ".";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese Numero Subred";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese IP de red";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(487, 78);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(74, 21);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "CLASE";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(487, 130);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(105, 21);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "MASCARA";
+            // 
+            // textClass
+            // 
+            this.textClass.Enabled = false;
+            this.textClass.Location = new System.Drawing.Point(584, 74);
+            this.textClass.Name = "textClass";
+            this.textClass.Size = new System.Drawing.Size(132, 20);
+            this.textClass.TabIndex = 3;
+            // 
+            // textMask
+            // 
+            this.textMask.Enabled = false;
+            this.textMask.Location = new System.Drawing.Point(598, 133);
+            this.textMask.Name = "textMask";
+            this.textMask.Size = new System.Drawing.Size(118, 20);
+            this.textMask.TabIndex = 4;
+            // 
+            // TablaSubred
+            // 
+            this.TablaSubred.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TablaSubred.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.TablaSubred.Location = new System.Drawing.Point(26, 257);
+            this.TablaSubred.Name = "TablaSubred";
+            this.TablaSubred.Size = new System.Drawing.Size(689, 258);
+            this.TablaSubred.TabIndex = 5;
+            this.TablaSubred.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.TablaSubred_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "N°";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "ID_RED";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Rango_IP_Configurable";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Brodcast";
+            this.Column4.Name = "Column4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(734, 523);
+            this.Controls.Add(this.TablaSubred);
+            this.Controls.Add(this.textMask);
+            this.Controls.Add(this.textClass);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Calculadora IP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TablaSubred)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textClass;
+        private System.Windows.Forms.TextBox textMask;
+        private System.Windows.Forms.DataGridView TablaSubred;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.TextBox textSub;
+    }
+}
+
---
+++ Subneteo/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Bitmap img = new Bitmap(Application.StartupPath + @"\img\fondo2.jpg");
+            this.BackgroundImage = img;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TablaSubred_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            Double RangoIp = Convert.ToDouble(textBox1.Text);
+            //************************  PARA LA CLASE A **********************************
+            if (RangoIp >= 0 && 128 > RangoIp)
+            {
+                textMask.Text = "255.0.0.0";
+                textClass.Text = "A";
+                CargandoSubred();
+            }
+            //************************  PARA LA CLASE B **********************************
+            else if (RangoIp >= 128 && 192 > RangoIp)
+            {
+                textMask.Text = "255.255.0.0";
+                textClass.Text = "B";
+                CargandoSubred();
+            }
+            //************************  PARA LA CLASE C **********************************
+            else if (RangoIp >= 192 && 224 > RangoIp)
+            {
+                textMask.Text = "255.255.255.0";
+                textClass.Text = "C";
+                CargandoSubred();
+            }
+            else if (RangoIp >= 224)
+            {
+                MessageBox.Show("Numero Fuera de Rango de Ip", " Ip de red", MessageBoxButtons.OKCancel);
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                textSub.Text = "";
+            }
+            //************************** CARGANDO LAS SUBREDES EN LA TABLA ****************************
+        }
+        public void CargandoSubred()
+        {
+            //***** numero de subred total: 0,2,6,14,30,62 --> (2^i )- 2 = resultado
+            Double NumSubredTotal = 0;
+            Double CantSubred = 0;
+            Double NumSubred = 0;
+            int SubredIngresada = Convert.ToInt32(textSub.Text);
+            for (int i = 1; i < 9; i++)
+            {
+                //******************TOTAL DE SUBRED *****************
+                NumSubredTotal = (Math.Pow(2, i) - 2);
+                if (SubredIngresada <= NumSubredTotal)
+                {
+                    //************* cantidad de bits utilizados *******************
+                    CantSubred = Math.Pow(2, 8 - i);
+                    NumSubred = Math.Pow(2, i);
+
+                    listarId_Red(CantSubred, NumSubred);
+                    listarId_Broadcast(NumSubred, CantSubred);
+                    listarIp_Configurable(NumSubred, CantSubred);
+                    pintarSubred(SubredIngresada);
+                    break;
+                }
+            }
+        }
+
+        //************************** LISTANDO LOS DATOS DE LA ID_RED ****************************
+
+        Double conthost1;
+        Double conthost2;
+        Double conthost3;
+        String ipRed = "";
+        public void listarId_Red(Double CatSubred, Double NumSubred)
+        {
+            conthost1 = 0;
+            conthost2 = 0;
+            conthost3 = 0;
+            for (int i = 0; i < NumSubred; i++)
+            {
+                //************************PARA LA CLASE A *************************
+                if (textClass.Text.Equals("A"))
+                {
+                    ipRed = textBox1.Text + "." + Convert.ToString(conthost1) + "." + Convert.ToString(conthost2) + "." + Convert.ToString(conthost3);
+                    conthost1 = conthost1 + CatSubred;
+                }
+                //************************PARA LA CLASE B *************************
+                else if (textClass.Text.Equals("B"))
+                {
+                    ipRed = textBox1.Text + "." + textBox2.Text + "." + Convert.ToString(conthost2) + "." + Convert.ToString(conthost3);
+                    conthost2 = conthost2 + CatSubred;
+                }
+                //************************PARA LA CLASE C *************************
+                else if (textClass.Text.Equals("C"))
+                {
+                    ipRed = textBox1.Text + "." + textBox2.Text + "." + textBox3.Text + "." + Convert.ToString(conthost3);
+                    conthost3 = conthost3 + CatSubred;
+                }
+
+                TablaSubred.Rows.Add();
+                TablaSubred.Rows[i].Cells[0].Value = i;
+                TablaSubred.Rows[i].Cells[1].Value = ipRed;
+
+            }
+        }
+
+        ////************************** LISTANDO LOS DATOS DE LA RANGO_IP_CONFIGURABLE ****************************
+
+        String ipRed1 = "";
+        String ipRed2 = "";
+        public void listarIp_Configurable(Double NumSubred, Double CatSubred)
+        {
+            conthost1 = 0;
+            conthost2 = 0;
+            conthost3 = 0;
+
+            for (int i = 0; i < NumSubred; i++)
+            {
+                //************************PARA LA CLASE A *************************
+                if (textClass.Text.Equals("A"))
+                {
+                    ipRed1 = textBox1.Text + "." + Convert.ToString(conthost1) + "." + Convert.ToString(conthost2) + "." + Convert.ToString(conthost3 + 1);
+                    conthost1 = conthost1 + CatSubred;
+                    ipRed2 = textBox1.Text + "." + Convert.ToString(conthost1 - 1) + "." + Convert.ToString(conthost2 + 255) + "." + Convert.ToString((conthost3 + 255) - 1);
+                }
+                //************************PARA LA CLASE B *************************
+                else if (textClass.Text.Equals("B"))
+                {
+                    ipRed1 = textBox1.Text + "." + textBox2.Text + "." + Convert.ToString(conthost2) + "." + Convert.ToString(conthost3 + 1);
+                    conthost2 = conthost2 + CatSubred;
+                    //contultimo = contultimo + CatSubred;
+                    ipRed2 = textBox1.Text + "." + textBox2.Text + "." + Convert.ToString(conthost2 - 2) + "." + Convert.ToString((conthost3 + 255) - 1);
+                }
+                //************************PARA LA CLASE C *************************
+                else if (textClass.Text.Equals("C"))
+                {
+                    ipRed1 = textBox1.Text + "." + textBox2.Text + "." + textBox3.Text + "." + Convert.ToString(conthost3 + 1);
+                    conthost3 = conthost3 + CatSubred;
+                    ipRed2 = textBox1.Text + "." + textBox2.Text + "." + textBox3.Text + "." + Convert.ToString(conthost3 - 2);
+                }
+
+                String ipRangoRed = ipRed1 + " -- " + ipRed2;
+                TablaSubred.Rows.Add();
+                TablaSubred.Rows[i].Cells[2].Value = ipRangoRed;
+
+            }
+
+        }
+
+        //************************** LISTANDO LOS DATOS DE LA ID_BROADCAST ****************************
+
+        public void listarId_Broadcast(Double NumSubred, Double CatSubred)
+        {
+            conthost1 = 0;
+            conthost2 = 0;
+            conthost3 = 0;
+            for (int i = 0; i < NumSubred; i++)
+            {
+                //************************PARA LA CLASE A *************************
+                if (textClass.Text.Equals("A"))
+                {
+                    conthost1 = conthost1 + CatSubred;
+                    ipRed = textBox1.Text + "." + Convert.ToString(conthost1 - 1) + "." + Convert.ToString(conthost3 + 255) + "." + Convert.ToString(conthost3 + 255);
+                }
+                //************************PARA LA CLASE B *************************
+                else if (textClass.Text.Equals("B"))
+                {
+                    conthost2 = conthost2 + CatSubred;
+                    ipRed = textBox1.Text + "." + textBox2.Text + "." + Convert.ToString(conthost2 - 1) + "." + Convert.ToString(conthost3 + 255);
+                }
+                //************************PARA LA CLASE C *************************
+                else if (textClass.Text.Equals("C"))
+                {
+                    conthost3 = conthost3 + CatSubred;
+                    ipRed = textBox1.Text + "." + textBox2.Text + "." + textBox3.Text + "." + Convert.ToString(conthost3 - 1);
+                }
+                TablaSubred.Rows.Add();
+                TablaSubred.Rows[i].Cells[3].Value = ipRed;
+
+            }
+        }
+
+        //************************** PINTANDO LA CANTIDAD DE SUBREDES DE LA TABLA ****************************
+
+        public void pintarSubred(double subred)
+        {
+            for (int i = 0; i < subred; i++)
+            {
+                // TablaSubred.Rows[i + 1].DefaultCellStyle.Equals(BackColor = Color.Red);
+                TablaSubred.Rows[i + 1].DefaultCellStyle.BackColor = Color.Orange;
+                //TablaSubred.RowsDefaultCellStyle.BackColor = Color.Blue;
+            }
+        }
+
+
+
+
+
+    }
+}
+
